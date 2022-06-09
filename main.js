ccec6ea2ab7be69c88ee74893181a5477237879e
Andarov <bekturandarob678@gmail.com>
2022-06-09 17:51:22 +0500

    Add cart counter

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const elMinus = document.querySelector('.js-minus')
-const elPlus = document.querySelector('.js-plus')
-const elCounting = document.querySelector('.hero__quantity')
+const elMinus = document.querySelector('.js-minus');
+const elPlus = document.querySelector('.js-plus');
+const elCounting = document.querySelector('.hero__quantity');
+const elsChangingImg = document.querySelectorAll(".js-img");
 
 elPlus.addEventListener('click', function(){
     elCounting.textContent = + elCounting.textContent + 1
@@ -8,9 +9,16 @@ elPlus.addEventListener('click', function(){
 
 elMinus.addEventListener('click', function(){
     if(elCounting.textContent > 0){
-        elCounting.textContent = + elCounting.textContent -1
+        elCounting.textContent = + elCounting.textContent -1;
     } else {
         elCounting.textContent = 0;
     }
 });
 
+elsChangingImg.forEach(function (item) {
+    item.addEventListener("click", function () {
+        document.querySelector(".hero__main-img").src = item.children[0].src
+    })
+})
+
+console.log(document.querySelector(".hero__main-img").src)
